94b299647517b7c92207f33401cfd845ffbe2dbe
Melanie Gilman <melaniegilman259@yahoo.com>
2020-08-22 13:16:05 -0400

    tables and values

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employeeDb;
+CREATE DATABASE employeeDb;
+USE employeeDb;
+
+
+
+CREATE TABLE department (
+ id INTEGER UNSIGNED AUTO_INCREMENT,
+ department_name VARCHAR(30) NOT NULL,
+ PRIMARY KEY (id) 
+);
+
+CREATE TABLE employee_role (
+  id INTEGER UNSIGNED AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INTEGER UNSIGNED,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INTEGER UNSIGNED AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER UNSIGNED,
+  manager_id INTEGER UNSIGNED,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE CASCADE,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (department_name)
+VALUES
+  ('Human Resources'),
+  ('Sales'),
+  ('Engineering'),
+  ('Research and Development'),
+  ('Finance'),
+  ('Operations');
+  
+
+INSERT INTO employee_role (title, salary, department_id)
+VALUES
+ ('Data Analyst', 80000, 4),
+ ('FrontEnd Developer', 50000, 3),
+ ('Manager', 90000, 4),
+ ('Sales Representative', 65000, 2),
+ ('Software Engineer', 70000, 3),
+ ('HR Manager', 70000, 1),
+ ('Operations Manager', 80000, 6),
+ ('Accountant', 85000, 5);
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Wibur', 'Basset', 3, null),
+  ('Melanie', 'Gilman', 2, 1),
+  ('Mohican', 'Beagle', 5, 1),
+  ('Grant', 'Emerson', 6, 3),
+  ('Mallory', 'Korpics', 7, 3),
+  ('Janelle', 'Riaubia', 8, 6),
+  ('John', 'Cox', 1, 6),
+  ('Chop', 'Suey', 4, 2),
+  ('Shine', 'Beta', 6, 1); 
